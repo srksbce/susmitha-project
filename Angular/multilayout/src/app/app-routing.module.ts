a63ceee64c62ac56deb428b9912c39e1c423a20a
susmitha28 <saisusmitha28@gmail.com>
2022-09-14 09:49:39 +0530

    Multilayout1

---
+++ Angular/multilayout/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { AuthenticationLayoutComponent } from './layouts/authentication-layout/authentication-layout.component';
+import { ADMIN_ROUTES } from './routing/admin-routing';
+import { AUTHENTICATION_ROUTES } from './routing/authentication-routing';
+
+const routes: Routes = [
+  {path:'', component: AuthenticationLayoutComponent, children:AUTHENTICATION_ROUTES},
+{path:'admin', component: AdminLayoutComponent, children:ADMIN_ROUTES}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ Angular/multilayout/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  ApiUrl:"https://testapi.skilllens.com/api/"
+  
+};
